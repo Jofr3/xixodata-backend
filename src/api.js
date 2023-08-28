7801c4c7ad220c6b8eb58f0a09c76d20e078366d
Fr3D0o <jofrescari@gmail.com>
2023-08-28 18:06:59 +0200

    modified app.js

--- src/api.js
+++ src/api.js
@@ -6,8 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/test', (req, res) => {
+  res.send("YOUUUUUUUUUUUUUUUUUU")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-// module.exports = app;
