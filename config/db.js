35ccf2c83c0f3db1b89628c2d6d5fddbf290ccfa
Fr3D0o <jofrescari@gmail.com>
2023-09-12 11:08:27 +0200

    db prod

--- config/db.js
+++ config/db.js
@@ -2,23 +2,23 @@ import { createConnection } from 'mysql2/promise';
 
 // Production
 
-// const connection = await createConnection({
-//     host: 'containers-us-west-152.railway.app',
-//     user: 'root',
-//     database: 'railway',
-//     password: 'Mp6U2ean2WVcrfFHWyTO',
-//     port: 6668
-// });
+const connection = await createConnection({
+    host: 'containers-us-west-152.railway.app',
+    user: 'root',
+    database: 'railway',
+    password: 'Mp6U2ean2WVcrfFHWyTO',
+    port: 6668
+});
 
 
 // Development
 
-const connection = await createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'your_database_name_here',
-    password: 'your_root_password_here',
-    port: 3306
-});
+// const connection = await createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'your_database_name_here',
+//     password: 'your_root_password_here',
+//     port: 3306
+// });
 
 export default connection
